331177650ce8ec886ca2b7935ee0a12952c05866
Nathan Koch <nathanrkoch@gmail.com>
2019-11-20 15:47:21 -0500

    refactored params to be better integrated with class

--- lib/sequence.lua
+++ lib/sequence.lua
@@ -7,6 +7,20 @@ local SEQ_PARAMS = include("lib/sequence_params")
 local CV_UTILS = include("lib/cv_utils")
 local DEFAULT_LENGTH = 8
 
+local function get_value_for_step(step)
+    return params:get(step.params.value)
+end
+
+local function get_active_for_step(step)
+    local active_state = params:get(step.params.active)
+    -- 1 = OFF, 2 = ON
+    if active_state == 1 then
+        return false
+    elseif active_state == 2 then
+        return true
+    end
+end
+
 function Sequence.new(idx, length)
     local letters = {"A", "B"}
     local step_names = {}
@@ -22,6 +36,7 @@ function Sequence.new(idx, length)
     for i = 1, s.length do
         s.steps[i] = {
             index = i,
+            params = {},
             slider = SEQ_UI.create_slider(i, layout),
             toggle = SEQ_UI.create_toggle(i, layout),
             highlight = SEQ_UI.create_highlight(i, layout)
@@ -32,6 +47,7 @@ function Sequence.new(idx, length)
     s.scale = nil
     s.octave = 0
     s.speed = 1
+    s.params = {}
     SEQ_UI.update_steps(s.steps, s.tabs.index, s.current_step)
 
     setmetatable(s, Sequence)
@@ -39,31 +55,18 @@ function Sequence.new(idx, length)
 end
 
 function Sequence:build_params()
-    SEQ_PARAMS.create_sequence_params(self)
-    SEQ_PARAMS.create_step_value_params(self)
-    SEQ_PARAMS.create_step_active_params(self)
-end
-
-function Sequence:get_value_for_step(step_num)
-    local param_id = SEQ_PARAMS.param_id_for_step_value(self, step_num)
-    return params:get(param_id)
-end
-
-function Sequence:get_active_for_step(step_num)
-    local active_state = params:get(SEQ_PARAMS.param_id_for_step_active(self, step_num))
-    -- 1 = OFF, 2 = ON
-    if active_state == 1 then
-        return false
-    elseif active_state == 2 then
-        return true
-    end
+    SEQ_PARAMS.build_sequence_params(self)
+    SEQ_PARAMS.build_step_value_params(self)
+    SEQ_PARAMS.build_step_active_params(self)
 end
 
 function Sequence:play_current_step()
-    local is_active = self:get_active_for_step(self.current_step)
+    local current_step = self.steps[self.current_step]
+
+    local is_active = get_active_for_step(current_step)
     local cv = nil
     if is_active then
-        cv = self:get_value_for_step(self.current_step) + self.octave
+        cv = get_value_for_step(current_step) + self.octave
         if self.scale then
             local note = CV_UTILS.quantize(cv, self.scale)
             cv = CV_UTILS.n2v(note)
@@ -105,7 +108,8 @@ function Sequence:select_step_by_delta(delta)
 end
 
 function Sequence:set_step_value_by_delta(step_idx, delta)
-    params:delta(SEQ_PARAMS.param_id_for_step_value(self, step_idx), delta)
+    local step = self.steps[step_idx]
+    params:delta(step.params.value, delta)
 end
 
 function Sequence:set_selected_step_value_by_delta(delta)
@@ -113,15 +117,16 @@ function Sequence:set_selected_step_value_by_delta(delta)
 end
 
 function Sequence:toggle_step(step_idx)
+    local step = self.steps[step_idx]
     -- TODO something seems overcomplicated in my toggles
-    local state = params:string(SEQ_PARAMS.param_id_for_step_active(self, step_idx))
+    local state = params:string(step.params.active)
     local new_state
     if state == "OFF" then
         new_state = 2
     else
         new_state = 1
     end
-    params:set(SEQ_PARAMS.param_id_for_step_active(self, step_idx), new_state)
+    params:set(step.params.active, new_state)
 end
 
 function Sequence:toggle_selected_step()
@@ -145,12 +150,14 @@ end
 function Sequence:set_cv_range(new_range)
     for _, step in ipairs(self.steps) do
         -- update param with a new max
-        local param = params:lookup_param(SEQ_PARAMS.param_id_for_step_value(self, step.index))
-        param.controlspec.maxval = new_range
+        -- not sure about editing this at runtime
+        -- but it is working fine
+        local step_value_param = params:lookup_param(step.params.value)
+        step_value_param.controlspec.maxval = new_range
 
         -- update slider UI with a new max
         step.slider.max_value = new_range
-        local slider_val = params:get(SEQ_PARAMS.param_id_for_step_value(self, step.index))
+        local slider_val = params:get(step.params.value)
         step.slider:set_value(slider_val)
     end
 end
--- lib/sequence_params.lua
+++ lib/sequence_params.lua
@@ -1,4 +1,3 @@
--- TODO refactor this entire module
 local CV_UTILS = include("lib/cv_utils")
 
 local CV_RANGE_OPTIONS = {"1V", "2V", "5V", "8V"}
@@ -12,64 +11,41 @@ CV_RANGE_REV_LOOKUP[8] = 4
 local CV_BEHAVIOR_OPTIONS = {"LINEAR", "MINOR", "MAJOR", "CHROMATIC"}
 local CV_BEHAVIOR_SCALES = {nil, CV_UTILS.scales.harmonicMinor, CV_UTILS.scales.major, CV_UTILS.scales.chromatic}
 
-local function param_id_for_step_value(sequence, step_num)
-    return "seq_" .. sequence.index .. "_" .. "step_" .. step_num .. "_value"
-end
+local OCTAVES = {"0", "1", "2", "3", "4", "5"}
 
-local function param_name_for_step_value(sequence, step_num)
-    return "seq " .. sequence.name .. ": " .. "step " .. step_num .. " value"
-end
-
-local function param_id_for_step_active(sequence, step_num)
-    return "seq_" .. sequence.index .. "_" .. "step_" .. step_num .. "_active"
-end
-
-local function param_name_for_step_active(sequence, step_num)
-    return "seq " .. sequence.name .. ": " .. "step " .. step_num .. " active"
-end
-
-local function param_for_cv_range(sequence)
-    return "seq_" .. sequence.index .. "_" .. "cv_range", "seq " .. sequence.name .. ": " .. "cv range"
-end
-
-local function param_for_cv_behavior(sequence)
-    return "seq_" .. sequence.index .. "_" .. "cv_behavior", "seq " .. sequence.name .. ": " .. "cv behavior"
-end
-
-local function param_for_octave(sequence)
-    return "seq_" .. sequence.index .. "_" .. "octave", "seq " .. sequence.name .. ": " .. "octave"
-end
-
-local function create_step_value_params(sequence)
+local function build_step_value_params(sequence)
     params:add_separator()
-
-    local cv_id, cv_name = param_for_cv_range(sequence)
-    local max_volts = CV_RANGE_VOLTAGES[params:get(cv_id)]
-
+    local max_volts = CV_RANGE_VOLTAGES[params:get(sequence.params.cv_range)]
     local linear_volts_cs = controlspec.new(0, max_volts, "lin", 0, 0, "volts")
     for _, step in ipairs(sequence.steps) do
+        local param_id = "seq_" .. sequence.index .. "_" .. "step_" .. step.index .. "_value"
+        local param_name = "seq " .. sequence.name .. ": " .. "step " .. step.index .. " value"
+
         params:add {
             type = "control",
-            id = param_id_for_step_value(sequence, step.index),
-            name = param_name_for_step_value(sequence, step.index),
+            id = param_id,
+            name = param_name,
             controlspec = linear_volts_cs,
             action = function(value)
                 step.slider:set_value(value)
             end
         }
+        step.params.value = param_id
     end
 end
 
-local function create_step_active_params(sequence)
+local function build_step_active_params(sequence)
     -- TODO there is both step active
     -- AND gate on the SQ-1
     -- so there needs to also be a gate param
     params:add_separator()
     for _, step in ipairs(sequence.steps) do
+        local param_id = "seq_" .. sequence.index .. "_" .. "step_" .. step.index .. "_active"
+        local param_name = "seq " .. sequence.name .. ": " .. "step " .. step.index .. " active"
         params:add {
             type = "option",
-            id = param_id_for_step_active(sequence, step.index),
-            name = param_name_for_step_active(sequence, step.index),
+            id = param_id,
+            name = param_name,
             options = {"OFF", "ON"},
             default = 2,
             action = function(value)
@@ -80,16 +56,19 @@ local function create_step_active_params(sequence)
                 end
             end
         }
+        step.params.active = param_id
     end
 end
 
-local function create_sequence_params(sequence)
+local function build_sequence_params(sequence)
     params:add_separator()
-    local ri, rn = param_for_cv_range(sequence)
+
+    local cv_range_param_id = "seq_" .. sequence.index .. "_" .. "cv_range"
+    local cv_range_param_name = "seq " .. sequence.name .. ": " .. "cv range"
     params:add {
         type = "option",
-        id = ri,
-        name = rn,
+        id = cv_range_param_id,
+        name = cv_range_param_name,
         options = CV_RANGE_OPTIONS,
         default = 1,
         action = function(value)
@@ -97,11 +76,14 @@ local function create_sequence_params(sequence)
             sequence:set_cv_range(voltage)
         end
     }
-    local bi, bn = param_for_cv_behavior(sequence)
+    sequence.params.cv_range = cv_range_param_id
+
+    local cv_behavior_param_id = "seq_" .. sequence.index .. "_" .. "cv_behavior"
+    local cv_behavior_param_name = "seq " .. sequence.name .. ": " .. "cv behavior"
     params:add {
         type = "option",
-        id = bi,
-        name = bn,
+        id = cv_behavior_param_id,
+        name = cv_behavior_param_name,
         options = CV_BEHAVIOR_OPTIONS,
         default = 1,
         action = function(value)
@@ -109,27 +91,26 @@ local function create_sequence_params(sequence)
             sequence.scale = scale
         end
     }
+    sequence.params.cv_behavior = cv_behavior_param_id
 
-    local oi, on = param_for_octave(sequence)
-    local octaves = {"0", "1", "2", "3", "4", "5"}
+    local octave_param_id = "seq_" .. sequence.index .. "_" .. "octave"
+    local octave_param_name = "seq " .. sequence.name .. ": " .. "octave"
     params:add {
         type = "option",
-        id = oi,
-        name = on,
-        options = octaves,
+        id = octave_param_id,
+        name = octave_param_name,
+        options = OCTAVES,
         default = 1,
         action = function(value)
-            sequence.octave = tonumber(octaves[value])
+            local octave = tonumber(OCTAVES[value])
+            sequence.octave = octave
         end
     }
+    sequence.params.octave = octave_param_id
 end
 
 return {
-    create_step_value_params = create_step_value_params,
-    create_step_active_params = create_step_active_params,
-    create_sequence_params = create_sequence_params,
-    param_id_for_step_value = param_id_for_step_value,
-    param_name_for_step_value = param_name_for_step_value,
-    param_id_for_step_active = param_id_for_step_active,
-    param_name_for_step_active = param_name_for_step_active
+    build_step_value_params = build_step_value_params,
+    build_step_active_params = build_step_active_params,
+    build_sequence_params = build_sequence_params
 }
