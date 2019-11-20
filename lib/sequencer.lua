caf3c4ca1da2d3708afc8e7341d4501f7753fa4c
Nathan Koch <nathanrkoch@gmail.com>
2019-11-20 14:53:06 -0500

    moved top level code into a sequencer class

---
+++ lib/sequencer.lua
@@ -0,0 +1,175 @@
+local Sequencer = {}
+Sequencer.__index = Sequencer
+
+local Sequence = include("lib/sequence")
+
+function play_step(self, sequence)
+    local crow_output_map = {
+        {cv = 1, gate = 2},
+        {cv = 3, gate = 4}
+    }
+    local do_mirror_outputs = (self.mode == "ALTERNATING" or self.mode == "SUCCESSIVE")
+    local crow_cv_out = crow_output_map[sequence.index].cv
+    local crow_gate_out = crow_output_map[sequence.index].gate
+    local gate, cv = sequence:play_current_step()
+
+    if cv then
+        if do_mirror_outputs then
+            for i = 1, 2 do
+                crow.output[crow_output_map[i].cv].volts = cv
+            end
+        else
+            crow.output[crow_cv_out].volts = cv
+        end
+    end
+    if gate then
+        if do_mirror_outputs then
+            for i = 1, 2 do
+                crow.output[crow_output_map[i].gate].execute()
+            end
+        else
+            crow.output[crow_gate_out].execute()
+        end
+    end
+end
+
+local function step_parallel(self)
+    for _, active_sequence in ipairs(self.sequences) do
+        play_step(self, active_sequence)
+        active_sequence:advance(self.mode)
+    end
+end
+
+local function step_successive(self)
+    local active_sequence = self.sequences[self.current_sequence]
+    play_step(self, active_sequence)
+    local finished = active_sequence:advance(self.mode)
+    if finished == true then
+        if self.current_sequence == 1 then
+            self.current_sequence = 2
+        else
+            self.current_sequence = 1
+        end
+    end
+end
+
+local function step_alternating(self)
+    local active_sequence = self.sequences[self.current_sequence]
+    play_step(self, active_sequence)
+    active_sequence:advance(self.mode)
+    if self.current_sequence == 1 then
+        self.current_sequence = 2
+    else
+        self.current_sequence = 1
+    end
+end
+
+function Sequencer.new()
+    local s = {}
+    s.modes = {
+        "ALTERNATING",
+        "SUCCESSIVE",
+        "PARALLEL_REVERSING",
+        "PARALLEL",
+        "CV_DUTY",
+        "CV_SLIDE",
+        "CV_DUTY_RANDOM",
+        "RANDOM"
+    }
+
+    s.num_sequences = 2
+    s.sequences = {}
+    for i = 1, s.num_sequences do
+        s.sequences[i] = Sequence.new(i)
+    end
+
+    -- current_sequence is used by these modes:
+    -- ALTERNATING
+    -- SUCCESSIVE
+    s.current_sequence = nil
+
+    setmetatable(s, Sequencer)
+    return s
+end
+
+function Sequencer:step()
+    if self.mode:match("PARALLEL") then
+        step_parallel(self)
+    elseif self.mode == "ALTERNATING" then
+        step_alternating(self)
+    elseif self.mode == "SUCCESSIVE" then
+        step_successive(self)
+    end
+end
+
+function Sequencer:update_ui()
+    for _, sequence in ipairs(self.sequences) do
+        sequence:update_ui()
+    end
+end
+
+function Sequencer:reset()
+    for _, sequence in ipairs(self.sequences) do
+        sequence:reset()
+    end
+end
+
+function Sequencer:change_mode(mode_number)
+    self.mode = self.modes[mode_number]
+    self:reset()
+    if self.mode == "SUCCESSIVE" or self.mode == "ALTERNATING" then
+        self.current_sequence = 1
+    else
+        self.current_sequence = nil
+    end
+end
+
+function Sequencer:get_sequence(idx)
+    return self.sequences[idx]
+end
+
+function Sequencer:autoscroll_page(pages)
+    if self.autoscroll == true and self.current_sequence then
+        if pages.index ~= self.current_sequence then
+            pages.index = self.current_sequence
+        end
+    end
+end
+
+function Sequencer:build_params()
+    -- global level params on top
+    -- TODO add params for:
+    -- duty time, using internal/external clock, speed (if internal)
+    params:add {
+        type = "option",
+        id = "sequencer_mode",
+        name = "mode",
+        options = self.modes,
+        default = 4, -- default is "PARALLEL"
+        action = function(value)
+            self:change_mode(value)
+        end
+    }
+    local autoscroll_options = {"ON", "OFF"}
+    params:add {
+        type = "option",
+        id = "sequencer_autoscroll",
+        name = "autoscroll",
+        options = autoscroll_options,
+        default = 1, -- default is "ON"
+        action = function(value)
+            if autoscroll_options[value] == "ON" then
+                self.autoscroll = true
+            else
+                self.autoscroll = false
+            end
+        end
+    }
+
+    -- sequence and step level params
+    for _, sequence in ipairs(self.sequences) do
+        sequence:build_params()
+    end
+end
+
+return Sequencer
--- seq-1.lua
+++ seq-1.lua
@@ -14,115 +14,21 @@
 -- see everything in the sequencer level block in the manual
 
 local UI = require "ui"
-local Sequence = include("lib/sequence")
+local Sequencer = include("lib/sequencer")
 
-local pages, sequences
-local sequencer = {
-	modes = {
-		"ALTERNATING",
-		"SUCCESSIVE",
-		"PARALLEL_REVERSING",
-		"PARALLEL",
-		"CV_DUTY",
-		"CV_SLIDE",
-		"CV_DUTY_RANDOM",
-		"RANDOM"
-	},
-	current_sequence = 1,
-	num_sequences = 2
-}
-
-local function play_step_of_sequence(sequence, play_all)
-	local play_all_outputs = play_all or false
-
-	local crow_outputs = {
-		{cv = 1, gate = 2},
-		{cv = 3, gate = 4}
-	}
-	local crow_cv_out = crow_outputs[sequence.index].cv
-	local crow_gate_out = crow_outputs[sequence.index].gate
-
-	local gate, cv = sequence:play_current_step()
-	if cv then
-		if play_all_outputs then
-			for i = 1, 2 do
-				crow.output[crow_outputs[i].cv].volts = cv
-			end
-		else
-			crow.output[crow_cv_out].volts = cv
-		end
-	end
-	if gate then
-		if play_all_outputs then
-			for i = 1, 2 do
-				crow.output[crow_outputs[i].gate].execute()
-			end
-		else
-			crow.output[crow_gate_out].execute()
-		end
-	end
-end
-
-local function step_parallel(mode)
-	for i = 1, sequencer.num_sequences do
-		local sequence = sequences[i]
-		play_step_of_sequence(sequence)
-		sequence:advance(mode)
-	end
-end
-
-local function step_successive(mode)
-	local sequence = sequences[sequencer.current_sequence]
-	play_step_of_sequence(sequence, true)
-
-	local finished = sequence:advance(mode)
-	if finished == true then
-		if sequencer.current_sequence == 1 then
-			sequencer.current_sequence = 2
-		else
-			sequencer.current_sequence = 1
-		end
-	end
-end
-
-local function step_alternating(mode)
-	local sequence = sequences[sequencer.current_sequence]
-	play_step_of_sequence(sequence, true)
-	sequence:advance(mode)
-	if sequencer.current_sequence == 1 then
-		sequencer.current_sequence = 2
-	else
-		sequencer.current_sequence = 1
-	end
-end
+local pages
 
 local function step()
-	local autoscroll = params:string("sequencer_autoscroll")
-	local mode = params:string("sequencer_mode")
-	if mode:match("PARALLEL") then
-		step_parallel(mode)
-	elseif mode == "ALTERNATING" then
-		step_alternating(mode)
-	elseif mode == "SUCCESSIVE" then
-		step_successive(mode)
-	end
-	print(sequencer.current_sequence)
-	if autoscroll == "ON" then
-		if pages.index ~= sequencer.current_sequence then
-			pages.index = sequencer.current_sequence
-		end
-	end
-	sequences[pages.index]:update_ui()
+	seq1:step()
+	seq1:autoscroll_page(pages)
+	seq1:update_ui()
 	redraw()
 end
 
 function init()
 	pages = UI.Pages.new(1, 2)
-
-	sequences = {}
-	for i = 1, sequencer.num_sequences do
-		sequences[i] = Sequence.new(i)
-	end
+	seq1 = Sequencer.new()
+	seq1:build_params()
 
 	crow.ii.pullup(true)
 	-- crow input 1 is a clock requiring triggers
@@ -145,55 +51,28 @@ function init()
 	crow.output[4].slew = 0
 	crow.output[4].action = "pulse(0.1,5,1)"
 
-	-- TODO add params for:
-	-- duty time, using internal/external clock, speed (if internal)
-	params:add {
-		type = "option",
-		id = "sequencer_mode",
-		name = "mode",
-		options = sequencer.modes,
-		default = 4,
-		action = function(value)
-			for _, sequence in ipairs(sequences) do
-				sequence:reset()
-			end
-			if sequencer.modes[value] == "SUCCESSIVE" then
-				sequencer.current_sequence = 1
-			end
-		end
-	}
-
-	params:add {
-		type = "option",
-		id = "sequencer_autoscroll",
-		name = "autoscroll",
-		options = {"ON", "OFF"},
-		default = 1
-	}
-
-	for _, sequence in ipairs(sequences) do
-		sequence:build_params()
-	end
-
+	seq1:build_params()
 	params:default()
 end
 
 function enc(n, delta)
+	local sequence = seq1:get_sequence(pages.index)
 	if n == 1 then
 		pages:set_index_delta(delta, false)
-		sequences[pages.index]:update_ui()
+		seq1:update_ui()
 	elseif n == 2 then
-		sequences[pages.index]:select_step_by_delta(delta)
-		sequences[pages.index]:update_ui()
+		sequence:select_step_by_delta(delta)
+		seq1:update_ui()
 	elseif n == 3 then
-		sequences[pages.index]:set_selected_step_value_by_delta(delta)
+		sequence:set_selected_step_value_by_delta(delta)
 	end
 	redraw()
 end
 
 function key(n, z)
+	local sequence = seq1:get_sequence(pages.index)
 	if n == 2 and z == 1 then
-		sequences[pages.index]:toggle_selected_step()
+		sequence:toggle_selected_step()
 	end
 	redraw()
 end
@@ -201,6 +80,7 @@ end
 function redraw()
 	screen.clear()
 	pages:redraw()
-	sequences[pages.index]:draw()
+	local sequence = seq1:get_sequence(pages.index)
+	sequence:draw()
 	screen.update()
 end
